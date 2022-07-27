79914bd125ee79e403dd4c013d8f10f699ce44f8
Luan Victor <luanvitorlima2017@gmail.com>
2022-07-27 10:05:30 -0300

    Update batalhaaleatoria.py

--- batalhaaleatoria.py
+++ batalhaaleatoria.py
@@ -16,42 +16,42 @@ dano = random.randint(1, 20)
 print(f"Batalha contra {criatura}:")
 
 if "Lobisomem" in criatura:
-    print(f"o  Lobisomem possui {lobisomem} de vida.")
+    print(f"O Lobisomem possui {lobisomem} de vida.")
     if lobisomem <= dano:
         print(f"você deferiu {dano} de dano na criatura, então vc o derrotou")
     else:
         print(f"você deferiu {dano} de dano na criatura, então você não o derrotou")
         
 if "Ogro" in criatura:
-    print(f"o  Ogro possui {ogro} de vida.")
+    print(f"O Ogro possui {ogro} de vida.")
     if ogro <= dano:
         print(f"você deferiu {dano} de dano na criatura, então vc o derrotou")
     else:
         print(f"você deferiu {dano} de dano na criatura, então você não o derrotou")
         
 if "Slime" in criatura:
-    print(f"o  Slime possui {slime} de vida.")
+    print(f"O Slime possui {slime} de vida.")
     if slime <= dano:
         print(f"você deferiu {dano} de dano na criatura, então vc o derrotou")
     else:
         print(f"você deferiu {dano} de dano na criatura, então você não o derrotou")
         
 if "Harpia" in criatura:
-    print(f"a Harpia possui {harpia} de vida.")
+    print(f"A Harpia possui {harpia} de vida.")
     if harpia <= dano:
         print(f"você deferiu {dano} de dano na criatura, então vc o derrotou")
     else:
         print(f"você deferiu {dano} de dano na criatura, então você não o derrotou")
 
 if "Vampiro" in criatura:
-    print(f"o  Vampiro possui {vampiro} de vida.")
+    print(f"O Vampiro possui {vampiro} de vida.")
     if vampiro <= dano:
         print(f"você deferiu {dano} de dano na criatura, então vc o derrotou")
     else:
         print(f"você deferiu {dano} de dano na criatura, então você não o derrotou")
         
 if "Goblin" in criatura:
-    print(f"o  Goblin possui {goblin} de vida.")
+    print(f"O Goblin possui {goblin} de vida.")
     if goblin <= dano:
         print(f"você deferiu {dano} de dano na criatura, então vc o derrotou")
     else:
